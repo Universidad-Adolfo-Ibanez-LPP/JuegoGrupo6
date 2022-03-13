a46eb7ba0ab632dfb5ca8a1a755e36b4956d6e9d
Casantibanezm <101525888+Casantibanezm@users.noreply.github.com>
2022-03-13 17:04:54 -0300

    Add files via upload

---
+++ Fin del juego.py
@@ -0,0 +1,7 @@
+if verificarpalabra: 
+   print('¡Ganaste! Encontraste el país oculto, felicitaciones')
+   break
+        
+else:
+  print(f'¡Perdiste! El país que debías encontrar era {país}.')   
+
