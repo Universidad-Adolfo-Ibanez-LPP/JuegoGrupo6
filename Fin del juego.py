68221cfda7cd739f6e337e267f0183cf3cbe3e8f
Casantibanezm <101525888+Casantibanezm@users.noreply.github.com>
2022-03-13 17:24:18 -0300

    Delete Fin del juego.py

--- Fin del juego.py
+++
@@ -1,7 +0,0 @@
-if verificarpalabra: 
-   print('¡Ganaste! Encontraste el país oculto, felicitaciones')
-   break
-        
-else:
-  print(f'¡Perdiste! El país que debías encontrar era {país}.')   
-
