ed9e0bbaf7a54d2507adfcf57534b99cbab0393d
Casantibanezm <101525888+Casantibanezm@users.noreply.github.com>
2022-03-13 17:27:22 -0300

    Add files via upload

---
+++ Findeljuego.py
@@ -0,0 +1,9 @@
+def findeljuegobueno(): 
+   print('¡Ganaste! Encontraste el país oculto, felicitaciones')
+        
+def findeljuegomalo(): 
+  print(f'¡Perdiste! El país que debías encontrar era {país}')
+
+def volverajugar():
+return input ('Si deseas jugar de nuevo presiona la letra S: ')
+
