5404accb4939bafb1dab70013eb3d26a21eed192
cristobalsepc <67245093+cristobalsepc@users.noreply.github.com>
2022-03-14 19:46:48 -0300

    Add files via upload

---
+++ paises_v2.py
@@ -0,0 +1,6 @@
+import random
+
+paises = ["argentina", "bolivia", "brasil", "colombia", "chile","ecuador", "guyana","paraguay", "peru","uruguay", "venezuela","surinam"]
+pais= random.choice(paises)
+def paises():
+  return pais
