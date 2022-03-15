021d4b4de5af68beb554eab7cd1f70a393d32d26
Daniel Bravo <101165561+Danbravo0@users.noreply.github.com>
2022-03-14 22:42:24 -0300

    Update verificación.py
    version mas ordenada y acorde a los demas

--- verificación.py
+++ verificación.py
@@ -1,17 +1,15 @@
-import findeljuego
-
 def verificacion_jugada (input_usuario, pais, aciertos, errores):
 
   if input_usuario in pais: #acierto
       print("Esta letra pertenece al pais")
       aciertos.append(input_usuario)
 
-      if len(aciertos)== len(set(pais)): #victoria
-        return findeljuego.findeljuegobueno()
+      if len(aciertos) > len(set(pais)): #victoria
+        return 1
 
   else: #fallo
       print("Esta letra NO pertenece al pais")
       errores.append(input_usuario)
 
-      if len(errores)==8: #derrota, el 8 es arbitrario
-        return findeljuego.findeljuegomalo()
+      if len(errores)>=5: #derrota, el 5 es arbitrario
+        return -1
