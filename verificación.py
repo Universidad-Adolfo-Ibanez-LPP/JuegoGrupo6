5a110e3270e687de024f73b6a5b3623767db073b
Daniel Bravo <101165561+Danbravo0@users.noreply.github.com>
2022-03-14 23:52:40 -0300

    Update verificación.py
    update 2

--- verificación.py
+++ verificación.py
@@ -4,7 +4,7 @@ def verificacion_jugada (input_usuario, pais, aciertos, errores):
       print("Esta letra pertenece al pais")
       aciertos.append(input_usuario)
 
-      if len(aciertos) > len(set(pais)): #victoria
+      if len(aciertos) >= len(set(pais)): #victoria
         return 1
 
   else: #fallo
