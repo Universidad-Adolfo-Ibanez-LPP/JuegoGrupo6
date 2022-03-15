6afe42d03f3eb5a8806a94ac4c2156bb85a7e1cf
cristobalsepc <67245093+cristobalsepc@users.noreply.github.com>
2022-03-14 22:45:15 -0300

    Delete paises_v2.py

--- paises_v2.py
+++
@@ -1,6 +0,0 @@
-import random
-
-paises = ["argentina", "bolivia", "brasil", "colombia", "chile","ecuador", "guyana","paraguay", "peru","uruguay", "venezuela","surinam"]
-pais= random.choice(paises)
-def paises():
-  return pais
