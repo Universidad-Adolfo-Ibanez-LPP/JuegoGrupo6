54bf0535cdca26242d0c67652a6b3bee850b9168
Casantibanezm <101525888+Casantibanezm@users.noreply.github.com>
2022-03-14 23:00:56 -0300

    Update Findeljuego.py

--- Findeljuego.py
+++ Findeljuego.py
@@ -1,11 +1,11 @@
 def findeljuegobueno(): 
    print('¡Ganaste! Encontraste el país oculto, felicitaciones')
-        
-def findeljuegomalo(): 
+
+def findeljuegomalo(país): 
   print(f'¡Perdiste! El país que debías encontrar era {país}')
 
 def volverajugar():
-return input ('Si deseas jugar de nuevo presiona ENTER: ')
+    return input ('Si deseas jugar de nuevo presiona ENTER: ')
 
 def contador(aciertos,errores):
   return print(f"Te demoraste {len(aciertos) + len(errores) - 1} intentos")
